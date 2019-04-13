68b76eae076304fb46f0af0bb329043a22ef806f
yuseon27 <ysnam19@naver.com>
2019-04-13 14:27:37 +0000

    First Commit

---
+++ get-user-info.php
@@ -0,0 +1,56 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    
+    include("dbcon.php");
+
+    $android = strpos($_SERVER["HTTP_USER_AGENT"], "Android");
+
+    error_log("TST", 0);
+
+    if( (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["submit"])) || $android )
+    {
+
+        $id=$_POST["id"];
+        $password=$_POST["password"];
+
+
+        // 안드로이드 코드의 posParameters 변수에 적어 준 이름을 가지고 값을 전달받습니다.
+        if (empty($id))
+            $errMSG = "아이디를 입력하세요.";
+        else if (empty($password))
+            $errMSG = "비밀번호를 입력하세요.";
+
+        if (!isset($errMSG)) {
+            try {
+                $stmt = $con->prepare("SELECT * FROM member WHERE ID=:id LIMIT 1");
+                $stmt->bindParam("id", $id);
+//		$stmt->bindParam("password", $password);
+                $stmt->execute();
+                $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+                $response = array();
+                $response["success"] = false;
+
+                if ($stmt->rowCount() > 0) {
+
+                    if(password_verify($password, $userRow["password"])) {
+//                        $_SESSION['id'] = $userRow['id'];
+                   
+                        $response["success"] = true;
+                        $response["id"] = $id;
+			$response["pw"] = $password;
+                    }
+                    
+                    header("Content-Type: application/jason; charset-utf8");
+                    echo json_encode($response, JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+
+                }
+
+            } catch (PDOException $e) {
+                 die("Database error : " .$e.getMessage());
+            }
+        }
+
+    }
+?>       
---
+++ insert-user.php
@@ -0,0 +1,97 @@
+<?php
+
+    error_reporting(E_ALL); 
+    ini_set("display_errors",1); 
+
+    include("dbcon.php");
+
+
+    $android = strpos($_SERVER["HTTP_USER_AGENT"], "Android");
+
+
+    error_log("TST",0);
+
+    if( (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["submit"])) || $android )
+    {
+
+        // 안드로이드 코드의 postParameters 변수에 적어준 이름을 가지고 값을 전달 받습니다.
+
+        $id=$_POST["id"];
+        $password=$_POST["password"];
+        $name=$_POST["name"];
+        $phonenum=$_POST["phonenum"];
+        $school=$_POST["school"];
+
+	echo "Test";
+
+        if(empty($id)){
+            $errMSG = "아이디를 입력하세요.";
+        }
+        else if(empty($password)){
+            $errMSG = "비밀번호를 입력하세요.";
+        }
+
+        else if(empty($name)){
+            $errMSG = "이름을 입력하세요.";
+        }
+        else if (empty($phonenum)){
+            $errMSG = "전화번호를 입력하세요.";
+        }
+        else if (empty($school)){
+            $errMSG = "학교를 입력하세요.";
+        }
+
+        if(!isset($errMSG)) // 모두 입력이 되었다면 
+        {
+            try{
+                // SQL문을 실행하여 데이터를 MySQL 서버의 person 테이블에 저장합니다.
+                $hashed_pw = password_hash($password, PASSWORD_DEFAULT);
+		echo $hashed_pw;
+
+                $stmt = $con->prepare("INSERT INTO member(ID, password, name, phonenum, school) VALUES(:id, :hashed_pw, :name, :phonenum, :school)");
+                $stmt->bindParam(":id", $id);
+                $stmt->bindParam(":hashed_pw", $hashed_pw);
+	        $stmt->bindParam(":name", $name);
+                $stmt->bindParam(":phonenum", $phonenum);
+                $stmt->bindParam(":school", $school);
+
+                if($stmt->execute())
+                {
+                    $successMSG = "새로운 사용자를 추가했습니다.";
+                }
+                else
+                {
+                    $errMSG = "사용자 추가 에러";
+                }
+
+            } catch(PDOException $e) {
+                die("Database error: " . $e->getMessage()); 
+            }
+        }
+       
+    }
+
+?>
+
+
+<html>
+    <body>
+        <?php 
+        if (isset($errMSG)) echo $errMSG;
+        if (isset($successMSG)) echo $successMSG;
+
+	    $android = strpos($_SERVER["HTTP_USER_AGENT"], "Android");
+   
+        if( !$android )
+        ?>
+         <form action="<?php $_PHP_SELF ?>" method="POST">
+              ID: <input type = "text" id = "id" />
+              Password: <input type = "text" password = "password" />
+	      Name: <input type = "text" name = "name" />
+	      Phonenum: <input type = "text" phonenum = "phonenum" />
+	      School: <input type = "text" school = "school" />
+              <input type = "submit" name = "submit" />
+          </form>
+ 
+     </body>
+</html>
